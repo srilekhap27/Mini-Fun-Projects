e98eb53d66ab64b194f1b226acdb4b86463c1c53
srilekhap27 <srilekha2222@gmail.com>
2024-03-17 01:27:01 -0500

    Completed Pomorodo Code

--- Pomodoro_Clock/pomodoro.js
+++ Pomodoro_Clock/pomodoro.js
@@ -5,13 +5,15 @@ const startBtn = document.querySelector(".startBtn");
 const pauseBtn = document.querySelector(".pauseBtn");
 const resumeBtn = document.querySelector(".resumeBtn");
 const resetBtn = document.querySelector(".resetBtn");
+const pomoCountsDisplay = document.querySelector(".pomoCountsDisplay");
 
 // Given work time and break time for 25 and 5 min respectively. For counting down we multiply by 60
-const Work_time = 0.2 * 60;
-const Break_time = 0.1 * 60;
+const Work_time = 25 * 60;
+const Break_time = 5 * 60;
 let timerID = null;
 let oneCycleComplete = false; //work+breaktime i.e 30min
 let totalCount = 0;
+let paused = false;
 
 //Function to update title
 const updateTitle = (msg) =>{
@@ -24,11 +26,13 @@ const saveLocalCounts = () =>{
     localStorage.setItem("pomocounts",JSON.stringify(counts));
 }
 
-
 //Function to countdown
 const countDown = (time) =>{
     return () => {
-        timer.textContent = time;
+        const minutes = Math.floor(time/60).toString().padStart(2,"0");
+        const seconds = Math.floor(time % 60).toString().padStart(2,"0");
+
+        timer.textContent = `${minutes}:${seconds}`;
         time--;
         if (time < 0){
             stopTimer();
@@ -39,9 +43,10 @@ const countDown = (time) =>{
             }
             else{
                 updateTitle("Completed One Round of Cycle");
-                setTimeout(() => updateTitle("Staer Timer Again"), 2000);
+                setTimeout(() => updateTitle("Start Timer Again"), 2000);
                 totalCount++;
-                saveLocalCounts();            
+                saveLocalCounts(); 
+                showPomoCounts();           
             }
         }
     }
@@ -58,8 +63,46 @@ const stopTimer = (stopTime) =>{
     clearInterval(timerID);
     timerID = null;
 }
+
+const getTimeInSeconds = (timeString) =>{
+    const[min, sec] = timeString.split(":");
+    return min * 60 + sec;
+}
 // adding even listener to start button for custom responses
 startBtn.addEventListener("click", () =>{
     timerID = startTimer(Work_time);
     updateTitle("It's Working Time !!");
-})
+});
+
+// adding even listener to Reset button for custom responses
+resetBtn.addEventListener("click",()=>{
+    stopTimer();
+    timer.textContent = "25:00";
+});
+
+// adding even listener to Pause button for custom responses
+pauseBtn.addEventListener("click",()=>{
+    stopTimer();
+    paused = true;
+    updateTitle("Timer Paused")
+});
+
+// adding even listener to Resume button for custom responses
+resumeBtn.addEventListener("click",()=>{  
+   if( paused = true){
+    const currentTime = getTimeInSeconds(timer.textContent);
+    timerID = startTimer(currentTime);
+    paused = false;
+    (!oneCycleComplete) ? updateTitle("It's Work Time!") : updateTitle("It's Break Time");
+   }
+});
+
+//Function for displaying completed cycles from local storage
+const showPomoCounts = () =>{
+    const counts = JSON.parse(localStorage.getItem("pomocounts"));
+    if (counts > 0){
+        pomoCountsDisplay.style.display = "flex";
+    }
+    pomoCountsDisplay.firstElementChild.textContent = counts;
+}
+showPomoCounts();
